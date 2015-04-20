db5be1217f7f63792cedf29946d63305fafb051f
Aveline Thelen <aveline@mobify.com>
2015-04-20 16:09:43 -0700

    Basic png rating scale

---
+++ tests/visual/runner.js
@@ -0,0 +1,23 @@
+define(function(require) {
+    var dust = require('dustjs-component');
+    var text = require('text');
+    var tests = require('text!tests.dust');
+    require('stencil-spec/spec.template');
+
+    // Load and compile this component’s dust templates:
+    require('../../rating-scale.template');
+
+    // Define any context required for the tests:
+    var context = {
+        repo: 'https://github.com/mobify/stencil-rating-scale'
+    };
+
+    // Render
+    dust.renderSource(tests, context, function(err, out) {
+        if (!err) {
+            document.querySelector('body').innerHTML = out;
+        } else {
+            console.log(err);
+        }
+    });
+});
